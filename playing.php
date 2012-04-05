420d775fa52ff0eff0c693257e7bbccfac465893
Del <del@lotsofcode.com>
2012-04-05 20:31:19 +0100

    Cleanup, I removing error reporting and replacing the div

--- playing.php
+++ playing.php
@@ -1,16 +1,11 @@
-<?
-	ini_set('display_errors','On');
-	error_reporting(E_ALL);
-	include 'classes/tagcloud.php';
-?>
-
 <link rel="stylesheet" type="text/css" href="./css/tagcloud.css" />
 
-<!-- <div style="width: 200px"> -->
+<div style="width: 200px">
 
 	<?
+		include 'classes/tagcloud.php';
 		$cloud = new tagcloud();
-		
+
 		/* adding a string */
 		$cloud->addString("This is a tag-cloud script, written by Del Harvey. I wrote this tag-cloud class because I just love writing code.");
 
@@ -28,11 +23,11 @@
 		$cloud->setRemoveWords(array('del','harvey'));
 
 		/* more complex adding */
-    	$cloud->addWord(array('word' => 'php', 'url' => 'http://www.php.net', 'colour' => 1));
-    	$cloud->addWord(array('word' => 'ajax', 'url' => 'http://www.php.net', 'colour' => 2));
-    	$cloud->addWord(array('word' => 'css', 'url' => 'http://www.php.net', 'colour' => 3));
+		$cloud->addWord(array('word' => 'php', 'url' => 'http://www.php.net', 'colour' => 1));
+		$cloud->addWord(array('word' => 'ajax', 'url' => 'http://www.php.net', 'colour' => 2));
+		$cloud->addWord(array('word' => 'css', 'url' => 'http://www.php.net', 'colour' => 3));
 
-    	/* set the minimum length required */
+		/* set the minimum length required */
 		$cloud->setMinLength(3);
 
 		/* limiting the output */
